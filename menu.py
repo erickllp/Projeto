c7834323363d62b523cba13996cb25ab24705a31
erickllp <148597074+erickllp@users.noreply.github.com>
2025-09-21 19:00:17 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,16 +1,16 @@
 from sistema import Pesquisador, Participante, OrgaoAmbiental, BancoDeDados, AnalistaDeDados
+import random
+
 
 def menu():
     print("\n=== Sistema de Pesquisa Clínica e Produção Farmacêutica ===")
-    print("1 - Criar Relatório Científico")
-    print("2 - Gerenciar Participantes")
-    print("3 - Inserir Resultado de Pesquisa")
-    print("4 - Inserir Impacto Ambiental")
-    print("5 - Listar Banco de Dados")
-    print("6 - Análise de Dados")
+    print("1 - Parciente")
+    print("2 - Pesquisador")
     print("0 - Sair")
     return input("Escolha uma opção: ")
-
+    print("")
+    print("-------------------------------------------")
+    print("")
 
 def main():
     pesquisador = Pesquisador("Dr. João")
@@ -23,37 +23,38 @@ def main():
         opcao = menu()
 
         if opcao == "1":
-            relatorio = pesquisador.relatorio_cientifico()
-            banco.salvar(relatorio)
-            print("✅ Relatório criado e salvo.")
-
+            print("o você esta procurando?")
+            print("1 - criar cadastro")
+            print("2 - Verificar resultado")
+            print("0 - voltar para tela inical")
+            paciente = input("escolha: ")
+            if paciente =="1":
+                nome = input("Nome do participante: ")
+                idade = int(input("Idade do participante: "))
+                participantes.append(Participante(nome, idade))
+                dado = pesquisador.gerenciamento_pessoas(participantes)
+                banco.salvar(dado)
+                print("✅ Participante adicionado.")
+            elif paciente == "2":
+                eficacia = random.uniform(0, 100)
+                print(f"{eficacia:.2f}")
+                if eficacia > 80:
+                    print(f"✅ Eficácia aprovada com {eficacia:.2f}%")
+                    dado = pesquisador.resultado_pesquisa(f"{eficacia:.2f}%")
+                    banco.salvar(dado)
+                else:
+                    print("Eficacia insuficiente")    
         elif opcao == "2":
-            nome = input("Nome do participante: ")
-            idade = int(input("Idade do participante: "))
-            participantes.append(Participante(nome, idade))
-            dado = pesquisador.gerenciamento_pessoas(participantes)
-            banco.salvar(dado)
-            print("✅ Participante adicionado.")
+            senha = 123
+            acesso = input("ambiente restrito, informe senha para continuar: ")
+            if acesso == senha:
+                print("Acesso permitido")
+                print("o que deseja acessar: ")  #criar opcões de escolha caso informe a senha
+                                                 #escolhas = relatorios, ambiente, resultados       
+            else:
+                print("acesso negado")      #voltar para inicio, ou encerrar code
+            
 
-        elif opcao == "3":
-            resultado = input("Digite o resultado da pesquisa: ")
-            dado = pesquisador.resultado_pesquisa(resultado)
-            banco.salvar(dado)
-            print("✅ Resultado salvo.")
-
-        elif opcao == "4":
-            descricao = input("Descreva o impacto ambiental: ")
-            impacto = orgao.impacto_ambiental(descricao)
-            banco.salvar(impacto)
-            print("✅ Impacto ambiental registrado.")
-
-        elif opcao == "5":
-            print("\n=== Banco de Dados ===")
-            for dado in banco.listar_dados():
-                print(f"- {dado}")
-
-        elif opcao == "6":
-            print(analista.analisar(banco))
 
         elif opcao == "0":
             print("Saindo do sistema...")
